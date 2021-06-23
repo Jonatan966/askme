2bd86a90160e6cb4db65f71c63df8a9d48a7f062
Jonatan966 <jonatanfrederico@gmail.com>
2021-06-22 21:41:16 -0300

    feat: room page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { HomePage } from './pages/Home'
 import { NewRoomPage } from "./pages/NewRoom"
 import { AuthProvider } from './hooks/useAuth'
+import { RoomPage } from './pages/Room'
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Route path='/' component={HomePage} exact />
-        <Route path='/rooms/new' component={NewRoomPage} />
+        <Switch>
+          <Route path='/' component={HomePage} exact />
+          <Route path='/rooms/new' component={NewRoomPage} />
+          <Route path='/rooms/:id' component={RoomPage} />
+        </Switch>
       </AuthProvider>
     </BrowserRouter>
   )
