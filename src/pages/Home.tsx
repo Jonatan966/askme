d272836d0470f987495d6c5267419e550dc55812
Jonatan966 <jonatanfrederico@gmail.com>
2021-06-22 21:13:26 -0300

    feat: logic to enter a room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,20 @@
+import { FormEvent, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 
 import { Button } from '../components/Button'
+import { database } from '../services/firebase'
+import { useAuth } from '../hooks/useAuth'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
-import { useAuth } from '../hooks/useAuth'
 
 import '../styles/auth.scss'
 
 export function HomePage() {
   const history = useHistory()
   const { signInWithGoogle, user } = useAuth()
+  const [roomCode, setRoomCode] = useState('')
 
   async function handleCreateRoom() {
     if (!user) {
@@ -19,6 +22,24 @@ export function HomePage() {
     }
 
     history.push('/rooms/new')
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault()
+
+    if (roomCode.trim() === '') {
+      return
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+    if (!roomRef.exists()) {
+      // TODO: utilizar o toastify
+      alert('Room does not exists')
+      return
+    }
+
+    history.push(`/rooms/${roomCode}`)
   }
 
   return (
@@ -39,10 +60,12 @@ export function HomePage() {
           
           <span className='separator'>Ou entre em uma sala</span>
 
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input 
               type="text" 
               placeholder='Digite o código da sala'
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
