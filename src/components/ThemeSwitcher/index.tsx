5850029ca33f19df30f42bd64158b8cc297e4fc0
Jonatan966 <jonatanfrederico@gmail.com>
2021-06-24 20:24:52 -0300

    feat: theme switcher button

---
+++ src/components/ThemeSwitcher/index.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@components/Button";
+
+import moonImg from '@assets/images/moon.svg'
+import sunImg from '@assets/images/sun.svg'
+import { useTheme } from "@hooks/useTheme";
+
+export function ThemeSwitcher() {
+  const { toggleTheme, currentTheme } = useTheme()
+
+  const themes = {
+    dark: sunImg,
+    light: moonImg
+  }
+  
+  return (
+    <Button onClick={toggleTheme} title={`Trocar para tema ${currentTheme === 'light' ? 'escuro' : 'claro'}`}>
+      <img src={themes[currentTheme]} alt={currentTheme} />
+    </Button>
+  )
+}
