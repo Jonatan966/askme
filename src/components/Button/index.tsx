1ea621ed30328d21b6711511b7615598c6cb5274
Jonatan966 <jonatanfrederico@gmail.com>
2021-06-25 21:37:24 -0300

    feat: load spinner in button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,4 @@
+import { Spinner } from '@components/Spinner'
 import { useTheme } from '@hooks/useTheme'
 import { ButtonHTMLAttributes } from 'react'
 
@@ -5,14 +6,21 @@ import './styles.scss'
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   isOutlined?: boolean;
+  isLoading?: boolean;
 }
 
-export function Button({ isOutlined = false, ...props }: ButtonProps) {
+export function Button({ isOutlined = false, isLoading, ...props }: ButtonProps) {
   const { currentTheme } = useTheme()
 
   return (
     <button
       {...props}
+      {...(isLoading && {
+        children: <Spinner />,
+        type: 'button',
+        onClick: () => undefined,
+        disabled: false
+      })}
       className={`button ${isOutlined ? 'outlined' : ''} ${props.className ? props.className : ''} ${currentTheme}`}
     />
   )
