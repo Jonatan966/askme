2d36f6064bfdcaf6086113245e4ba0e7375353f8
Jonatan966 <jonatanfrederico@gmail.com>
2021-06-27 13:20:21 -0300

    feat: theme switcher label

--- src/components/ThemeSwitcher/index.tsx
+++ src/components/ThemeSwitcher/index.tsx
@@ -5,7 +5,11 @@ import { Button } from '@components/Button'
 import { ReactComponent as MoonImg } from '@assets/images/moon.svg'
 import { ReactComponent as SunImg } from '@assets/images/sun.svg'
 
-export function ThemeSwitcher() {
+interface ThemeSwitcherType {
+  showLabel?: boolean;
+}
+
+export function ThemeSwitcher({ showLabel = false }: ThemeSwitcherType) {
   const { toggleTheme, currentTheme } = useTheme()
 
   const themes = {
@@ -13,8 +17,15 @@ export function ThemeSwitcher() {
     light: <MoonImg/>
   }
 
+  const translatedCurrentTheme = currentTheme === 'light' ? 'escuro' : 'claro'
+
   return (
-    <Button onClick={toggleTheme} title={`Trocar para tema ${currentTheme === 'light' ? 'escuro' : 'claro'}`}>
+    <Button
+      onClick={toggleTheme}
+      title={`Trocar para tema ${translatedCurrentTheme}`}
+      isOutlined
+    >
+      {showLabel && `Trocar para tema ${translatedCurrentTheme}`}
       {themes[currentTheme]}
     </Button>
   )
